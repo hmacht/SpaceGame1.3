28518473b93e647e8031cb21862736345a10ed14
Toby Krieman <tobykreiman@Tobys-MacBook-Pro.local>
2018-03-05 17:02:35 -0500

    Fixed iPhone 6 and lower bug

--- Orbit/GameScene.swift
+++ Orbit/GameScene.swift
@@ -1373,9 +1373,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 }
             }
         }
-        
-        
-        
+        touchesMoved(touches, with: event)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
